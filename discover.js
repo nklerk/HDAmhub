2478d76808d9d8fef3216981f98366b22e0921cf
Niels de Klerk <mail@nielsdeklerk.nl>
2018-12-11 20:39:39 +0100

    only report uniquehostnames

--- discover.js
+++ discover.js
@@ -10,7 +10,9 @@ module.exports = {
 
 function discover() {
   return new Promise((resolve, reject) => {
+    let discoveredApi = [];
     let discovered = [];
+
     let mdnsBrowser = mdns.createBrowser("_http._tcp");
 
     mdnsBrowser.on("ready", function() {
@@ -19,15 +21,16 @@ function discover() {
     });
 
     mdnsBrowser.on("update", function(mdnsResponceData) {
-      if (isMdnsDataMhub(mdnsResponceData)) {
-        discovered.push(new hdaApi(mdnsResponceData.host));
+      if (isMdnsDataMhub(mdnsResponceData) && isUnique(discovered, mdnsResponceData.host)) {
+        discovered.push(mdnsResponceData.host);
+        discoveredApi.push(new hdaApi(mdnsResponceData.host));
       }
     });
 
     setTimeout(() => {
       mdnsBrowser.stop();
       console.log("Discovery:  Stop");
-      resolve(discovered);
+      resolve(discoveredApi);
     }, MDNS_TIMEOUT);
   });
 }
@@ -35,4 +38,8 @@ function discover() {
 //is data from a MHUB device?
 function isMdnsDataMhub(mdnsData) {
   return typeof mdnsData.txt == "object" && typeof mdnsData.txt[0] == "string" && mdnsData.txt[0].search("MHUB") == A_MATCH;
+}
+
+function isUnique(array, element) {
+  return array.indexOf(element) > -1;
 }
